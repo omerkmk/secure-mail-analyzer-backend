b16f91e63966cbd412bc00c9e5b03840ce243ebf
omerkmk03 <omerkumek03@gmail.com>
2026-07-02 22:01:21 +0300

    Initial Spring Boot backend setup

---
+++ src/main/java/com/securemail/analyzer/SecureMailAnalyzerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.securemail.analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureMailAnalyzerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureMailAnalyzerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/securemail/analyzer/SecureMailAnalyzerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securemail.analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureMailAnalyzerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
